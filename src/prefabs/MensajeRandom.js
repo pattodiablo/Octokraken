515423df78de9aa1ce07d4822a3c38521d3165a5
José Agustín Carrión Jaramillo <aguchocarrion@gmail.com>
2022-10-26 11:25:18 -0500

    [AUDIO] puesto dm alert pero no funca

--- src/prefabs/MensajeRandom.js
+++ src/prefabs/MensajeRandom.js
@@ -15,34 +15,48 @@ class MensajeRandom extends Phaser.GameObjects.Sprite {
 
 	/* START-USER-CODE */
 
+	/*create() {
+		this.dm_alert1 = this.sound.add('dm_alert1');
+		this.dm_alert1.loop = false;
+		this.dm_alert1.play();
+		
+    }*/
+
 	generarMensaje(){
 
+		
 		
 		this.cualMensaje = Phaser.Math.Between(1,6);
 		console.log("mensaje generado " +  this.cualMensaje)
 		switch(this.cualMensaje ){
 			case 1:
 				this.setTexture("mensaje1");
+				//this.dm_alert1.play();
 			break;
 
 			case 2:
 				this.setTexture("mensaje2");
+				//this.dm_alert1.play();
 			break;
 
 			case 3:
 				this.setTexture("mensaje3");
+				//this.dm_alert1.play();
 			break;
 
 			case 4:
 				this.setTexture("mensaje4");
+				//this.dm_alert1.play();
 			break;
 
 			case 5:
 				this.setTexture("mensaje5");
+				//this.dm_alert1.play();
 			break;
 
 			case 6:
 				this.setTexture("mensaje6");
+				//this.dm_alert1.play();
 			break;
 
 
